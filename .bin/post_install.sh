ee3c4d817c171a005d9a4eabb8fb487e9fbc703a
Isaac Gierard <isaac.gierard@juxt.com>
2015-03-27 15:42:36 -0700

    fix: updated bin install logic

--- .bin/post_install.sh
+++ .bin/post_install.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
-pwd
 TMP_FILE=/tmp/parse.tmp
 if [ -e /tmp/parse.tmp ]; then
   echo "Cleaning up from previous install failure"
   rm -f /tmp/parse.tmp
 fi
 echo "Fetching latest version ..."
-curl --progress-bar https://www.parse.com/downloads/cloud_code/parse -o /tmp/parse.tmp
+case `uname` in
+  "Linux" )
+    curl --progress-bar -Lo /tmp/parse.tmp https://parse.com/downloads/cloud_code/cli/parse-linux/latest
+    ;;
+  "Darwin" )
+    curl --progress-bar -Lo /tmp/parse.tmp https://parse.com/downloads/cloud_code/cli/parse-osx/latest
+    ;;
+esac
 echo "Installing ..."
 mv /tmp/parse.tmp ./.bin/parse
 chmod 755 ./.bin/parse
